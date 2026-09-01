932eb2bdd9600b71c4ad645104bb95c94725032d
Danayt <danaytgebregziabher@gmail.com>
2026-09-01 14:42:08 +0300

    frontend completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { Toaster } from 'react-hot-toast'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import AppLayOut from './pages/AppLayOut'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductPage from './pages/ProductPage'
+import SearchResults from './pages/SearchResults'
+import FlashDeals from './pages/FlashDeals'
+import Checkout from './pages/CheckOut'
+import MyOrders from './pages/MyOrders'
+import OrderTracking from './pages/OrderTracking'
+import Addresses from './pages/Addresses'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Toaster position="top-right" toastOptions={{duration: 3000, style: {
+      background: "#1B3022", color: "#fff", borderRadius: "12px", fontSize:
+      "14px" }}}/>
+
+      <Routes>
+        {/* Auth pages - No Navbar/Footer */}
+        <Route path='/login' element={<Login/>}/>
+
+        {/* Main pages - With Navbar/Footer */}
+        <Route path='/' element={<AppLayOut />}>
+          <Route index element={<Home />}/>
+          <Route path="products" element={<Products />}/>
+          <Route path="products/:id" element={<ProductPage />}/>
+          <Route path="search" element={<SearchResults />}/>
+          <Route path="deals" element={<FlashDeals />}/>
+          <Route element={<ProtectedRoute/>}>
+            <Route path="checkout" element={<Checkout />}/>
+            <Route path="orders" element={<MyOrders />}/>
+            <Route path="orders/:id" element={<OrderTracking />}/>
+            <Route path="addresses" element={<Addresses />}/>
+          </Route>
+        </Route>
+      </Routes>
+    </>
   )
 }
 
-export default App
+export default App;
