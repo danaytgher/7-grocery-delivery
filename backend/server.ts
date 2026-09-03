a4cd8a26703234867f1bc9a81db5909913bdb006
Danayt <danaytgebregziabher@gmail.com>
2026-09-03 12:20:48 +0300

    fix vercel backend

--- backend/server.ts
+++ backend/server.ts
@@ -30,6 +30,4 @@ app.use((error: any, req:Request, res: Response, next: NextFunction)=> {
     console.error(error)
     res.status(500).json({message: error.message})
 })
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+export default app;
