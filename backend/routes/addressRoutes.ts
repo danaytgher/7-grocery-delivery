7fe1c072cd3c7fb3322e6b7c873e215c2ae08d4f
Danayt <danaytgebregziabher@gmail.com>
2026-09-09 09:51:46 +0300

    project completed

--- backend/routes/addressRoutes.ts
+++ backend/routes/addressRoutes.ts
@@ -6,7 +6,7 @@ const addressRouter = express.Router()
 
 addressRouter.get('/', auth, getAddresses)
 addressRouter.post('/', auth, addAddress)
-addressRouter.put('/:_id', auth, updateAddress)
-addressRouter.delete('/:_id', auth, deleteAddress)
+addressRouter.put('/:id', auth, updateAddress)
+addressRouter.delete('/:id', auth, deleteAddress)
 
 export default addressRouter
